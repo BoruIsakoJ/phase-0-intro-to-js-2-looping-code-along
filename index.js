73775fddae13626350e85c6662eacc4242d63831
Boru Isako <isco.jaldy@gmail.com>
2025-02-25 17:43:02 +0300

    Complete labs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, events) {
+    let messages = []
+    for (let count = 0; count < names.length; count++) {
+        messages.push(`Thank you, ${names[count]}, for the wonderful ${events} gift!`)
+    }
+    return messages
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"))
+
+function countDown(num) {
+    for (let count = num; num >= 0; num--) {
+        console.log(num)
+
+    }
+}
+countDown(10)
